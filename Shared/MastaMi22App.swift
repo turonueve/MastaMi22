ebd79816bdf18f22697fc754eb087c4717e92c75
turonueve <97420253+turonueve@users.noreply.github.com>
2022-08-18 09:25:04 -0400

    Initial Commit

---
+++ Shared/MastaMi22App.swift
@@ -0,0 +1,17 @@
+//
+//  MastaMi22App.swift
+//  Shared
+//
+//  Created by Avalokiteshvara on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MastaMi22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
