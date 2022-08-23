7fb0f5bddc974804c9000ec816386327fdf9015d
turonueve <97420253+turonueve@users.noreply.github.com>
2022-08-23 13:26:02 -0700

    Pull down Menu

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+
+
+   
+
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 10) {
+            Menu("Select")
+            {
+                    Button("Option 1", action: performOption1)
+                    Button("Option 2", action: performOption2)
+                    Button("Option 3", action: performOption3)
+            }
+            
+        }
+        .padding()
     }
-}
+    func performOption1() {
+        print("This is Option1")
+    }
+    func performOption2() {
+        print("This is Option1")
+    }
+    func performOption3() {
+        print("This is Option1")
+    }
+ }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
